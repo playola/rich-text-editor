d29b7452ff7cacf0dbe0a779f14be1adc50c932c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-22 18:09:54 +0000

    Deploy playola/rich-text-editor to github.com/playola/rich-text-editor.git:gh-pages

---
+++ precache-manifest.1efb7c51a690e6835f9814e2e355568a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfbe61cfd2265c321b79d9e9623af1a",
+    "url": "/rich-text-editor/index.html"
+  },
+  {
+    "revision": "19d6115d28393769eec5",
+    "url": "/rich-text-editor/static/js/2.38522819.chunk.js"
+  },
+  {
+    "revision": "ee8c699fd7621e27d15cf851a1d39c7f",
+    "url": "/rich-text-editor/static/js/2.38522819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee0cdf4bb824de3280b",
+    "url": "/rich-text-editor/static/js/main.eca26c06.chunk.js"
+  },
+  {
+    "revision": "8be68f3830e8f9f8db1b",
+    "url": "/rich-text-editor/static/js/runtime-main.7deccce2.js"
+  }
+]);
